f5fa33b0ade54863a33101b9828a0afcb0a21ef3
Kylie Greenwald <kg208@duke.edu>
2022-07-12 14:18:03 -0400

    remove volunteer

--- root-causes-volunteer/react-volunteer-app/src/components/uploadImages/notes/context.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const NotesContext = React.createContext({
-  currentNote: null,
-  notes: [{ id: 1, text: "Type your note here!" }]
-});
-
-export default NotesContext;
