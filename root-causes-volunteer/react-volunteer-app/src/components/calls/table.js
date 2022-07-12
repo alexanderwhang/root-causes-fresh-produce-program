4ce7f19f865d3f2c19dfdc260661c455f62e95e2
Kylie Greenwald <kg208@duke.edu>
2022-07-12 17:24:02 -0400

    changed button styles

--- root-causes-volunteer/react-volunteer-app/src/components/calls/table.js
+++ root-causes-volunteer/react-volunteer-app/src/components/calls/table.js
@@ -222,8 +222,8 @@ function Row(props) {
                         <input type="hidden" name="id" value={row.id} />
                         <input style={{ marginRight: '15px' }} type= "text" name="notes" placeholder="Enter notes here..." />
                       <br />
-                      <Button
-                          style={{backgroundColor: "#00743e", marginTop: "10px", marginRight: "5px"}}
+                      <Button id="note_submit"
+                          style={{marginTop: "10px", marginRight: "5px"}}
                           type="submit"
                           variant="contained"
                           sx={{ mt: 3, mb: 2 }}
@@ -233,9 +233,9 @@ function Row(props) {
                       </Button>
                       <form noValidate method = "post" action="http://127.0.0.1:5000/calls/deletenotes">
                         <input type="hidden" name="id" value={row.id} />
-                        <Button
+                        <Button id="note_delete"
                             name = "delete"
-                            style={{backgroundColor: "#fc2848", marginTop: "10px"}}
+                            style={{marginTop: "10px"}}
                             type="submit"
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
