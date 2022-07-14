44db6b73858cf96c280fab61a6f82f2368b86b9e
Karen Gonzalez <karen.gonzalezpalomo@duke.edu>
2022-07-14 09:55:35 -0400

    two new columns added

--- root-causes-backend/app.py
+++ root-causes-backend/app.py
@@ -52,6 +52,8 @@ class Participant(db.Model):
     pronouns = db.Column(db.Text, nullable=True)
     group = db.Column(db.String(1), nullable=False)
     household_size = db.Column(db.Integer, nullable=True)
+    most_recent_delivery = db.Column(db.String(100), nullable=True)
+    most_recent_call = db.Column(db.String(100), nullable=True)
 
     children = relationship("Status")
     children = relationship("DeliveryHistory")
@@ -60,7 +62,7 @@ class Participant(db.Model):
     def __repr__(self):
         return f"Participant: {self.first_name} {self.last_name}"
 
-    def __init__(self, first_name, last_name, date_of_birth, age, phone, language, email, pronouns, group, household_size):
+    def __init__(self, first_name, last_name, date_of_birth, age, phone, language, email, pronouns, group, household_size, most_recent_delivery, most_recent_call):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
@@ -71,6 +73,8 @@ class Participant(db.Model):
         self.pronouns = pronouns
         self.group = group
         self.household_size = household_size
+        self.most_recent_delivery = most_recent_delivery
+        self.most_recent_call = most_recent_call
 
 def format_participant(participant):
     status = Status.query.filter_by(participant_id=participant.id).one()
